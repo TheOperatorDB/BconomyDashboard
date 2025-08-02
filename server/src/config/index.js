9916d55e5e8e9e329c51c320247282d499b1f4d5
Flavien Sellet <flavien.sellet@gmail.com>
2025-08-02 17:51:40 +0200

    Add client url env variable for CORS

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -5,5 +5,5 @@ export const config = {
   port: process.env.PORT || 3001,
   env: process.env.NODE_ENV || "development",
   apiKey: process.env.MY_BCONOMY_API_KEY,
-  allowedOrigin: "http://localhost:5173",
+  allowedOrigin: process.env.CLIENT_URL || "http://localhost:5173",
 };
